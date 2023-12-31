1b4ae0b180b7807329612302ad3bc4b2ad4e8d41
Asiel Cabrera <cabrerasiel@gmail.com>
2023-12-31 13:47:28 -0500

    adding test to event manager

--- Tests/Cardinal-SystemTests/EventManagerTest.swift
+++ Tests/Cardinal-SystemTests/EventManagerTest.swift
@@ -5,4 +5,23 @@
 //  Created by Asiel Cabrera Gonzalez on 12/31/23.
 //
 
-import Foundation
+import XCTest
+@testable import Cardinal_System
+
+final class EventManagerTest: XCTestCase {
+    struct TestEvent: Event {
+        var id: UUID
+        var name: String
+        var description: String
+ 
+    }
+    
+    func testCreateEvent() {
+        var eventManager = EventManager()
+        let id = UUID()
+        let event = TestEvent(id: id, name: "testEvent", description: "Event for testing proporses")
+        eventManager.createEvent(event)
+        XCTAssertEqual(eventManager.getAllEvent().count, 1)
+        XCTAssertEqual(eventManager.getEvent(id)?.name, "testEvent")
+    }
+}
