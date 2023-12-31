92ab5a5effebaee06d60e6d44abe307cda51e41c
Asiel Cabrera <cabrerasiel@gmail.com>
2023-12-31 13:48:59 -0500

    adding vapor dependencie

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "Cardinal-System",
+    platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Cardinal-System",
             targets: ["Cardinal-System"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Cardinal-System"),
+            name: "Cardinal-System",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+            ]
+        ),
         .testTarget(
             name: "Cardinal-SystemTests",
             dependencies: ["Cardinal-System"]),
