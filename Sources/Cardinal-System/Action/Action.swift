827170be293d00914a0cd369b930dc038e6ec0bc
Asiel Cabrera <cabrerasiel@gmail.com>
2024-01-02 17:15:31 -0500

    adding command protocol

--- Sources/Cardinal-System/Action/Action.swift
+++ Sources/Cardinal-System/Action/Action.swift
@@ -1,8 +1,13 @@
 //
-//  File.swift
-//  
+//  Action.swift
+//
 //
 //  Created by Asiel Cabrera Gonzalez on 1/2/24.
 //
 
 import Foundation
+
+public protocol Action {
+    associatedtype T: Event
+    static func execute(_ event: T) async throws
+}
--- Sources/Cardinal-System/Command/Command.swift
+++ Sources/Cardinal-System/Command/Command.swift
@@ -1,8 +1,12 @@
 //
-//  File.swift
-//  
+//  Command.swift
+//
 //
 //  Created by Asiel Cabrera Gonzalez on 12/31/23.
 //
 
 import Foundation
+
+public protocol Command: Information & Action {
+
+}
